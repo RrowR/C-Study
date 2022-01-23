d0473c9452591774db897d8a4b8af5ac0113a5da
Memo <2415782224@qq.com>
2022-01-23 16:47:27 +0800

    first c++

---
+++ day01/Hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// #include <stdio.h>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    std::cout << "hello world" << std::endl;
+    // printf("HelloWorld\n")
+    // cin.get();
+    return 0;
+}
